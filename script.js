9ceead3557f8d09f7192f3e904fca10f7bd2e654
Federico Peirano <fedepr2345@gmail.com>
2024-02-06 15:58:05 -0300

    Arreglo link a los proyectos

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
