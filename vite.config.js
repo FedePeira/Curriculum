fc208a5a54b9f11b167f8f4a2bae6b5dd23a40b6
FedePeira <fedepr2345@gmail.com>
2025-06-15 18:43:52 -0400

    V3.12 - Esta es la vencida

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        es: 'src/es/index.html',
+      },
+    },
   },
   publicDir: '../public',
-};
+});
